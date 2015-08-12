3eae2629d84772fe53be3e963db111444bc75e2b
Luis Javier Peris Morillo <javierperis@gmail.com>
2015-08-13 01:22:54 +0200

    Implementa CRUD de preguntas Autoload Añade temática a las preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,24 +1,119 @@
 var models = require('../models/models.js');
 
-// GET /quizes/question
-exports.question = function(req, res) {
-    models.Quiz.findAll().success(function(quiz) {	
-	res.render('quizes/question', {pregunta: quiz[0].pregunta});
-    })
+exports.load = function(req, res, next, quizId) {
+    models.Quiz.findById(quizId).then(
+	function(quiz) {
+	    if (quiz) {
+		req.quiz = quiz;
+		next();
+	    }
+	    else {
+		next(new Error('No existe quizId=' + quizId));
+	    }
+	}
+    ).catch(function(error) { next(error);});
+};
+
+
+exports.index = function(req, res) {
+    if (req.query.search) {
+	var search = '%' + req.query.search.replace(/\s/g,"%") + '%';
+
+	models.Quiz.findAll(
+	    { where: ["pregunta like ?", search],
+	      order: [['pregunta', 'ASC']]}
+	).then(function(quizes) {
+	    res.render('quizes/index', {quizes: quizes, errors: []});
+	}).catch(function(error) {next(error);});
+    } else {    
+	models.Quiz.findAll().then(function(quizes) {
+	    res.render('quizes/index', { quizes: quizes, errors: []});
+	}).catch(function(error) { next(error);});
+    }
+};
+
+exports.show = function(req, res) {
+    res.render('quizes/show', {quiz: req.quiz, errors: []});
 };
 
 // GET /quizes/answer
 exports.answer = function(req, res) {
-    models.Quiz.findAll().success(function(quiz) {	
-	if (req.query.respuesta === quiz[0].respuesta) {
-	    res.render('quizes/answer', {respuesta: 'Correcto'});
-	} else {
-	    res.render('quizes/answer', {respuesta: 'Incorrecto'});
+    var resultado = 'Incorrecto';
+    if (req.query.respuesta === req.quiz.respuesta) {
+	resultado = 'Correcto';
+    } 
+    res.render(
+	'quizes/answer',
+	{ quiz: req.quiz,
+	  respuesta: resultado,
+	  errors: []
 	}
-    })
+    );
+};
+
+exports.new = function(req, res) {
+    var quiz = models.Quiz.build(
+	{ pregunta: "Pregunta",
+	  respuesta: "Respuesta",
+	  tema: "Temática"
+	}
+    );
+    res.render('quizes/new', {quiz: quiz, errors: []});
+};
+
+exports.create = function(req, res) {
+    var quiz = models.Quiz.build( req.body.quiz );
+
+    quiz
+	.validate()
+	.then(
+	    function(err) {
+		if(err) {
+		    res.render('quizes/new', {quiz: quiz, errors: err.errors});
+		} else {
+		    quiz
+			.save({fields: ["pregunta", "respuesta", "tema"]})
+			.then(function() {
+			    res.redirect('/quizes');
+			})
+		}
+	    }
+	);
+};
+
+exports.edit = function(req, res) {
+    var quiz = req.quiz;
+    res.render('quizes/edit', {quiz: quiz, errors: []});
+};
+
+exports.update = function(req, res) {
+    req.quiz.pregunta = req.body.quiz.pregunta;
+    req.quiz.respuesta = req.body.quiz.respuesta;
+    req.quiz.tema = req.body.quiz.tema;
+
+    req.quiz
+	.validate()
+	.then(
+	    function(err) {
+		if (err) {
+		    res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
+		}
+		else {
+		    req.quiz
+			.save( {fields: ["pregunta", "respuesta", "tema"]})
+			.then( function() { res.redirect('/quizes');});
+		}
+	    }
+	);
+};
+
+exports.destroy = function(req, res) {
+    req.quiz.destroy().then( function() {
+	res.redirect('/quizes');
+    }).catch(function(error){next(error)});
 };
 
 // GET /author
 exports.author = function(req, res) {
-    res.render('author', {});
+    res.render('author', {errors: []});
 };
